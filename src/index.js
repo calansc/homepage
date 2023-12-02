7fe466aef08ccf12490923307f4b5b4fb91ed4e4
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-12-02 10:42:37 -0500

    Add filler content and begin formatting for mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+// eslint-disable-next-line no-unused-vars
 import { css } from "./style.css";
+import manatee from "./manatee.jpg";
+import manatee2 from "./manatee2.jpg";
+
+let headerPictureFrame = document.querySelector(".header-picture-frame");
+let headerPicture = new Image();
+headerPicture.classList.add("header-picture");
+headerPicture.src = manatee2;
+headerPictureFrame.appendChild(headerPicture);
+// console.log(headerPicture);
+
+let projectImg = document.querySelectorAll(".project-img");
+let projectScreenshot = new Image();
+projectScreenshot.classList.add("project-screenshot");
+projectScreenshot.src = manatee;
+projectImg[0].appendChild(projectScreenshot);
+console.log(projectImg);
